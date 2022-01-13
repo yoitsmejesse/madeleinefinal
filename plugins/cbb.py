c94ae9a7f827b7886a60effeb8d1d5e5eaeb1bcf
yoitsmejesse <95560554+yoitsmejesse@users.noreply.github.com>
2022-01-13 22:14:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : <a href='t.me/showsarchive'>Cine Verse Archive</a>\n○ Support Group : <a href='t.me/movieshowsrequestgroup'>Req Movies | Shows</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : Python3\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : <a href='t.me/showsarchive'>Cine Verse Archive</a>\n○ Support Group : <a href='t.me/movieshowsrequestgroup'>Req Movies | Series</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
